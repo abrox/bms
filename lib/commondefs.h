c4b077ec08cf2a596424f14b99fef478868b4b9d
Jukka-Pekka Sarjanen <jp.sarjanen@gmail.com>
2019-01-02 19:46:53 +0200

    Move message definations to own header

--- lib/commondefs.h
+++ lib/commondefs.h
@@ -4,6 +4,7 @@
 #include <runnable.h>
 #include "RTClib.h"
 
+#include "msg.h"
 #define DEBUG  
 #ifdef DEBUG  
   #define DPRINT(...)    Serial.print(__VA_ARGS__) 
@@ -13,23 +14,6 @@
   #define DPRINTLN(...)
 #endif
 
-enum class Msg{
-    DISPLAY_OFF_TIMEOUT,
-    UPDATE_STATUS_DISLAY,
-    BUTTON_DOWN,
-    BUTTON_DOWN_TWO_SEC,
-    BUTTON_DOWN_LONG,
-    BUTTON_DOWN_EXTLONG,
-    BUTTON_UP,
-    MEASSURE_TIMEOUT,
-    CURRENT_TIMEOUT,
-    START_MQTT,
-    MQTT_CLIENT_TIMEOUT,
-    MQTT_RUNNING,
-    MQTT_FAILED,
-    SOC_UPDATE,
-    CURRENT_REQ
-};
 
 enum class BootType{
     UNDEFINED,
