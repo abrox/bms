4beee4525747089ae27e1dacb0ba597790335822
Jukka-Pekka Sarjanen <jp.sarjanen@gmail.com>
2019-02-22 22:06:57 +0200

    Refactoring: Move debug print to own function

--- lib/currentsensor.cpp
+++ lib/currentsensor.cpp
@@ -1,5 +1,35 @@
 #include "currentsensor.h"
 
+//Debug printout
+void printCurrent(INA219& monitor){
+    DPRINT("raw shunt voltage: ");
+    DPRINTLN(monitor.shuntVoltageRaw());
+
+    DPRINT("raw bus voltage:   ");
+    DPRINTLN(monitor.busVoltageRaw());
+
+    DPRINTLN("--");
+
+    DPRINT("shunt voltage: ");
+    DPRINT(monitor.shuntVoltage() * 1000, 4);
+    DPRINTLN(" mV");
+
+    DPRINT("shunt current: ");
+    DPRINT(monitor.shuntCurrent() * 1000, 4);
+    DPRINTLN(" mA");
+
+    DPRINT("bus voltage:   ");
+    DPRINT(monitor.busVoltage(), 4);
+    DPRINTLN(" V");
+
+    DPRINT("bus power:     ");
+    DPRINT(monitor.busPower() * 1000, 4);
+    DPRINTLN(" mW");
+
+    DPRINTLN(" ");
+    DPRINTLN(" ");
+
+}
 
 CurrentSensor::CurrentSensor(eQueue_t &eq,const INA219::t_i2caddr &addr):Runnable(eq),monitor(addr)
 {
@@ -8,39 +38,14 @@ CurrentSensor::CurrentSensor(eQueue_t &eq,const INA219::t_i2caddr &addr):Runnabl
 
 void CurrentSensor::handleMsgIn(const Msg &msg)
 {
-   if( msg == Msg::CURRENT_TIMEOUT ||
-       msg == Msg::CURRENT_REQ ){
-      Serial.print("raw shunt voltage: ");
-  Serial.println(monitor.shuntVoltageRaw());
-  
-  Serial.print("raw bus voltage:   ");
-  Serial.println(monitor.busVoltageRaw());
+    if( msg == Msg::CURRENT_TIMEOUT ||
+        msg == Msg::CURRENT_REQ ){
 
-  Serial.println("--");
+        _appCtx->_currentData.volt = monitor.busVoltage();
+        _appCtx->_currentData.cu   = monitor.shuntCurrent();
 
-  Serial.print("shunt voltage: ");
-  Serial.print(monitor.shuntVoltage() * 1000, 4);
-  Serial.println(" mV");
-
-  Serial.print("shunt current: ");
-  Serial.print(monitor.shuntCurrent() * 1000, 4);
-  Serial.println(" mA");
-
-  Serial.print("bus voltage:   ");
-  Serial.print(monitor.busVoltage(), 4);
-  Serial.println(" V");
-
-  Serial.print("bus power:     ");
-  Serial.print(monitor.busPower() * 1000, 4);
-  Serial.println(" mW");
-
-  Serial.println(" ");
-  Serial.println(" ");
-
-  _appCtx->_currentData.volt = monitor.busVoltage();
-  _appCtx->_currentData.cu   = monitor.shuntCurrent();
-
-  _eq.putQ(Msg::SOC_UPDATE);
+        printCurrent(monitor);
+        _eq.putQ(Msg::SOC_UPDATE);
    }
   
 }
